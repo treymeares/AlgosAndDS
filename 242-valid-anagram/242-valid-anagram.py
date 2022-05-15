9a1146a13c1165c329a49b035322c23815bda0fb
Trey Meares <59393122+treymeares@users.noreply.github.com>
2022-05-15 15:45:02 -0400

    Time: 4802 ms (5.01%), Space: 14 MB (63.62%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         if len(s) != len(t):
             return False
-        for letter in set(s):
+        for letter in s:
             if s.count(letter) != t.count(letter):
                 return False
         return True
