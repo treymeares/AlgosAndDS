2d441a08751932ddfc69b7a4dc9f560f2384f03a
Trey Meares <59393122+treymeares@users.noreply.github.com>
2022-05-15 15:50:04 -0400

    Time: 83 ms (87.82%), Space: 17.3 MB (89.78%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -5,14 +5,22 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        seen = {}
-        for words in strs:
-            x = "".join(sorted(words))
-            if x not in seen:
-                seen[x] = [words]
-            else:
-                seen[x].append(words)
-        return list(seen.values())   
-            
+        # seen = {}
+        # for words in strs:
+        #     x = "".join(sorted(words))
+        #     if x not in seen:
+        #         seen[x] = [words]
+        #     else:
+        #         seen[x].append(words)
+        # return list(seen.values())   
+    
         
+        seen = {}
+        for word in strs:
+            joined = "".join(sorted(word))
+            if joined not in seen:
+                seen[joined] = [word]
+            else:
+                seen[joined].append(word)
+        return list(seen.values())
         
