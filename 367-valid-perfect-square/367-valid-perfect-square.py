2ce3f6d991cba1c9e6a19d54d060027d2ae3256c
Trey Meares <59393122+treymeares@users.noreply.github.com>
2022-05-15 21:20:40 -0400

    Time: 19 ms (72.22%), Space: 13.3 MB (84.06%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num < 2:
+            return True
+        
+        left, right = 2, num // 2
+        
+        while left <= right:
+            x = left + (right - left) // 2
+            guess_squared = x * x
+            if guess_squared == num:
+                return True
+            if guess_squared > num:
+                right = x - 1
+            else:
+                left = x + 1
+        
+        return False
+        
