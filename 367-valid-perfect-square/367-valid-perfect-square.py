531fa6cc9ab5023af53790c3c28111e2834de7e6
Trey Meares <59393122+treymeares@users.noreply.github.com>
2022-05-15 21:28:08 -0400

    Time: 36 ms (16.30%), Space: 13.4 MB (35.64%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.py
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -7,17 +7,18 @@ class Solution(object):
         if num < 2:
             return True
         
-        left, right = 2, num // 2
-        
+        left = 2
+        right = num // 2
         while left <= right:
-            x = left + (right - left) // 2
-            guess_squared = x * x
-            if guess_squared == num:
+            square = (left + right) // 2
+            match = square * square
+            if match == num:
                 return True
-            if guess_squared > num:
-                right = x - 1
+            elif match < num:
+                left = square + 1
             else:
-                left = x + 1
-        
+                right = square - 1
+                
         return False
-        
+    
+    
