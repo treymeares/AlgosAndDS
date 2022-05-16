e5b1933c4bf4308ca86d5e850184b1b6d4717cbc
Trey Meares <59393122+treymeares@users.noreply.github.com>
2022-05-15 22:07:32 -0400

    Time: 32 ms (25.89%), Space: 13.4 MB (84.26%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -1,12 +1,12 @@
 class Solution(object):
     def areAlmostEqual(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
+        c=0
         if sorted(s1) != sorted(s2):
             return False
-        
-        #return true if there is no difference (strings are already equal) or there is difference of two which can be swaped
-        return sum([1 if s1[i] != s2[i] else 0 for i in range(len(s1))]) in [0, 2]
+        for i in range(len(s1)):
+            if s1[i] == s2[i]:
+                c= c + 1
+        if c >= len(s1) - 2:
+            return True
+        else:
+            return False
