6e6bfdd8375cc3337e4356171f8d67a59f74809e
Trey Meares <59393122+treymeares@users.noreply.github.com>
2022-05-15 22:17:30 -0400

    Time: 43 ms (7.61%), Space: 13.4 MB (63.45%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -18,25 +18,6 @@ class Solution(object):
         else:
             return False
             
-            
-            
-    #     class Solution(object):
-    # def areAlmostEqual(self, s1, s2):
-    #     """
-    #     :type s1: str
-    #     :type s2: str
-    #     :rtype: bool
-    #     """
-    #     c=0
-    #     if sorted(s1)!= sorted(s2):
-    #         return False
-    #     for i in range(len(s1)):
-    #         if s1[i] == s2[i]:
-    #             c=c+1
-    #     if c >= len(s1)-2:
-    #         return True
-    #     else:
-    #         return False
         
         
         
