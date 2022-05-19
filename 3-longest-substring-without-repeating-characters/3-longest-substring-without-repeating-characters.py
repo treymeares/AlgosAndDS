fe9be71c78c66b6a7954db10d658d224a09ae105
Trey Meares <59393122+treymeares@users.noreply.github.com>
2022-05-19 19:08:41 -0400

    Time: 92 ms (52.31%), Space: 18.8 MB (5.65%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,32 +1,33 @@
-# class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         result = []
-#         string = ""
-#         for idx, x in enumerate(s):
-#             if x not in string:
-#                 string += x
-#             else:
-#                 result.append(string)
-#                 string = string[idx + 1:]
-#                 string += x
-#         result.append(string)
-#         return len(max(result, key = len))
-    
-    
-    
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max_s = []
-        tmp_s = ''
-        for i in s:
-            if i not in tmp_s:
-                tmp_s += i
+        result = []
+        string = ""
+        for idx, x in enumerate(s):
+            if x not in string:
+                string += x
             else:
-                max_s.append(tmp_s)
-                loc = tmp_s.find(i)
-                tmp_s = tmp_s[loc+1:]
-                tmp_s += i
+                result.append(string)
+                location = string.find(x)
+                string = string[location + 1:]
+                string += x
+        result.append(string)
+        return len(max(result, key = len))
+    
+    
+    
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         max_s = []
+#         tmp_s = ''
+#         for i in s:
+#             if i not in tmp_s:
+#                 tmp_s += i
+#             else:
+#                 max_s.append(tmp_s)
+#                 loc = tmp_s.find(i)
+#                 tmp_s = tmp_s[loc+1:]
+#                 tmp_s += i
 
-        max_s.append(tmp_s)
+#         max_s.append(tmp_s)
         
-        return len(max(max_s, key=len))
+#         return len(max(max_s, key=len))
