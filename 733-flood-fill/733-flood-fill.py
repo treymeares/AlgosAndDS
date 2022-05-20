a40fb73eae278a32f8e02df2e47bb6e87b390e0a
Trey Meares <59393122+treymeares@users.noreply.github.com>
2022-05-19 20:27:32 -0400

    Time: 163 ms (5.18%), Space: 14 MB (90.42%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,12 +1,40 @@
-class Solution(object):
-    def floodFill(self, image, sr, sc, newColor):
-        rows, cols, orig_color = len(image), len(image[0]), image[sr][sc]
-        def traverse(row, col):
-            if (not (0 <= row < rows and 0 <= col < cols)) or image[row][col] != orig_color:
-                return
-            image[row][col] = newColor
-            [traverse(row + x, col + y) for (x, y) in ((0, 1), (1, 0), (0, -1), (-1, 0))]
-        if orig_color != newColor:
-            traverse(sr, sc)
-        return image
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        color = image[sr][sc]
+        rows = len(image)
+        cols = len(image[0])
         
+        if color == newColor:
+            return image
+        
+        visited = [((sr, sc))]
+        unvisited = [(sr, sc)]
+        
+        while len(unvisited) > 0:
+            r, c = unvisited.pop()
+            
+            if r >= rows or r < 0 or c >= cols or c < 0:
+                continue
+            
+            if image[r][c] == color:
+                image[r][c] = newColor
+            else:
+                continue
+                
+            if (r+1, c) not in visited:
+                    visited.append((r+1, c))
+                    unvisited.append((r+1, c))
+                    
+            if (r, c+1) not in visited:
+                    visited.append((r, c+1))
+                    unvisited.append((r, c+1))
+                    
+            if (r, c-1) not in visited:
+                    visited.append((r, c-1))
+                    unvisited.append((r, c-1))
+                    
+            if (r-1, c) not in visited:
+                    visited.append((r-1, c))
+                    unvisited.append((r-1, c))
+                    
+        return image
