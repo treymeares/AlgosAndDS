7262edbaaf33a1fde353226508ce49a6cc72a803
Trey Meares <59393122+treymeares@users.noreply.github.com>
2022-05-19 20:24:35 -0400

    Time: 85 ms (76.55%), Space: 14 MB (68.27%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,20 +1,21 @@
-from itertools import permutations
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-	    cntr, w, match = Counter(s1), len(s1), 0     
-
-	    for i in range(len(s2)):
-		    if s2[i] in cntr:
-			    if not cntr[s2[i]]: match -= 1
-			    cntr[s2[i]] -= 1
-			    if not cntr[s2[i]]: match += 1
-
-		    if i >= w and s2[i-w] in cntr:
-			    if not cntr[s2[i-w]]: match -= 1
-			    cntr[s2[i-w]] += 1
-			    if not cntr[s2[i-w]]: match += 1
-
-		    if match == len(cntr):
-			    return True
-
-	    return False
+        dct = collections.defaultdict(int)
+        comp = collections.Counter(s1)
+        
+        l = 0
+        
+        for r, ch in enumerate(s2):
+            dct[ch] += 1
+            
+            if r - l + 1 == len(s1):
+                if dct == comp: 
+                    return True
+                dct[s2[l]] -= 1
+                
+                if dct[s2[l]] == 0:
+                    del dct[s2[l]]
+                
+                l += 1
+        
+        return False
