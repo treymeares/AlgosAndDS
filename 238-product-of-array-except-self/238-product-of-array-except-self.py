1581556ec5bcc3eada5cddc3c0a23d3f87c4a0fd
Trey Meares <59393122+treymeares@users.noreply.github.com>
2022-06-01 21:56:32 -0400

    Time: 405 ms (18.68%), Space: 21 MB (81.00%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -6,20 +6,11 @@ class Solution:
         for x in range(len(nums)):
             output[x] = prefix
             prefix *= nums[x]
+        
         postfix = 1
         for x in range(len(nums) -1, -1, -1):
             output[x] *= postfix
             postfix *= nums[x]
+        
         return output
             
-# res = [1] * (len(nums))
-        
-#         prefix = 1
-#         for i in range(len(nums)):
-#             res[i] = prefix
-#             prefix *= nums[i]
-#         postfix = 1
-#         for i in range(len(nums) - 1, -1, -1):
-#             res[i] *= postfix
-#             postfix *= nums[i]
-#         return res
